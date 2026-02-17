d9bfd258cb15f264e2eab190afd58a94594b49fd
Vinayak Bhawsar <admin@vinayak.com>
2026-02-17 22:39:40 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -4,18 +4,17 @@ import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { SearchContextProvider } from "./context/SearchContext";
 import { BrowserRouter } from "react-router-dom";
-import Header from "./components/header/Header";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
   <React.StrictMode>
-    <AuthContextProvider>
-      <SearchContextProvider>
-        <App />
-        <Header />
-      </SearchContextProvider>
-    </AuthContextProvider>
-  </React.StrictMode>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <SearchContextProvider>
+          <App />
+        </SearchContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
+  </React.StrictMode>
 );
