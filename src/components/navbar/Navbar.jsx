54c24aec9dfc7ec6cf6d7f9f0c095d12fb4d7f54
Vinayak Bhawsar <admin@vinayak.com>
2026-02-17 20:57:09 +0530

    fixed navbar routing

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import "./navbar.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
+
 const Navbar = () => {
   const { user } = useContext(AuthContext);
 
@@ -11,10 +12,18 @@ const Navbar = () => {
         <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
           <span className="logo">lamabooking</span>
         </Link>
-        {user ? user.username : (
+
+        {user ? (
+          <span>{user.username}</span>
+        ) : (
           <div className="navItems">
-            <button className="navButton">Register</button>
-            <button className="navButton">Login</button>
+            <Link to="/register">
+              <button className="navButton">Register</button>
+            </Link>
+
+            <Link to="/login">
+              <button className="navButton">Login</button>
+            </Link>
           </div>
         )}
       </div>
