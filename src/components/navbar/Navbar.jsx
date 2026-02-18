f462532dc9d98b8be85806a69942201e658f6ae7
Vinayak Bhawsar <admin@vinayak.com>
2026-02-18 22:45:26 +0530

    updates

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,28 +1,49 @@
 import "./navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
 const Navbar = () => {
-  const { user } = useContext(AuthContext);
+  const { user, dispatch } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+    navigate("/");
+  };
 
   return (
     <div className="navbar">
       <div className="navContainer">
-        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+        <Link
+          to="/"
+          style={{ color: "inherit", textDecoration: "none" }}
+        >
           <span className="logo">lamabooking</span>
         </Link>
 
         {user ? (
-          <span>{user.username}</span>
+          <div className="navItems">
+            <span className="username">Hi, {user.username}</span>
+            <button
+              className="navButton"
+              onClick={handleLogout}
+            >
+              Logout
+            </button>
+          </div>
         ) : (
           <div className="navItems">
             <Link to="/register">
-              <button className="navButton">Register</button>
+              <button className="navButton">
+                Register
+              </button>
             </Link>
 
             <Link to="/login">
-              <button className="navButton">Login</button>
+              <button className="navButton">
+                Login
+              </button>
             </Link>
           </div>
         )}
