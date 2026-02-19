83b9a4d8e9d7ba6788de640ca29c9acf77062d6d
Vinayak Bhawsar <admin@vinayak.com>
2026-02-19 12:31:33 +0530

    updates

--- src/components/propertyList/PropertyList.jsx
+++ src/components/propertyList/PropertyList.jsx
@@ -1,8 +1,10 @@
 import useFetch from "../../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 import "./propertyList.css";
 
 const PropertyList = () => {
   const { data, loading } = useFetch("/hotels/countByType");
+  const navigate = useNavigate();
 
   const images = [
     "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
@@ -11,6 +13,11 @@ const PropertyList = () => {
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
     "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
   ];
+
+  const handleClick = (type) => {
+    navigate(`/hotels?type=${type}`);
+  };
+
   return (
     <div className="pList">
       {loading ? (
@@ -18,16 +25,19 @@ const PropertyList = () => {
       ) : (
         <>
           {data &&
-            images.map((img,i) => (
-              <div className="pListItem" key={i}>
-                <img
-                  src={img}
-                  alt=""
-                  className="pListImg"
-                />
+            images.map((img, i) => (
+              <div
+                className="pListItem"
+                key={i}
+                onClick={() => handleClick(data[i]?.type)}
+                style={{ cursor: "pointer" }}
+              >
+                <img src={img} alt="" className="pListImg" />
                 <div className="pListTitles">
                   <h1>{data[i]?.type}</h1>
-                  <h2>{data[i]?.count} {data[i]?.type}</h2>
+                  <h2>
+                    {data[i]?.count} {data[i]?.type}
+                  </h2>
                 </div>
               </div>
             ))}
